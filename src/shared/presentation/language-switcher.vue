53e06cce57c4273f2a49eee265f9cb4425e756a6
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-18 16:43:59 -0500

    feat(i18n): add language switcher component.

---
+++ src/shared/presentation/language-switcher.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <pv-select-button 
+      v-model="$i18n.locale" 
+      :options="$i18n.availableLocales.map(locale => ({ label: locale.toUpperCase(), value: locale }))" />
+</template>
+
+<style scoped>
+
+</style>
