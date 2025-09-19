39d28e7031f806d8c5d7a2c30cd6d8fa378b4270
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-19 18:18:34 -0500

    feat(news): add article list component.

---
+++ src/news/presentation/components/article-list.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {Article} from "../../domain/model/article.entity.js";
+import ArticleItem from "./article-item.vue";
+
+const props = defineProps({ articles: Array[Article], required: true });
+const { articles } = props;
+</script>
+
+<template>
+  <div v-for="article in articles">
+    <article-item :article="article"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
